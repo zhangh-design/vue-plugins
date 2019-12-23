51d2a62b128c0e14d90c0de8fdd26230fa7568e8
zhanghao <506365426@qq.com>
2019-12-23 15:24:29 +0800

    feat: 自定义指令v-f-ref

---
+++ plugins/v-f-ref/index.js
@@ -0,0 +1,60 @@
+import _get from 'lodash/get'
+import _isEqual from 'lodash/isEqual'
+import _find from 'lodash/find'
+import _isNil from 'lodash/isNil'
+import _isFunction from 'lodash/isFunction'
+import _isEmpty from 'lodash/isEmpty'
+
+/**
+ * @desc
+ * 获取跨层级子孙组件实例
+ */
+export default {
+  install: function (Vue, options) {
+    const directiveName = _get(options, 'name', 'f-ref')
+    Vue.directive(directiveName, {
+      bind (el, binding, vNode) {
+        console.info('bind');
+        // 初始化
+        const callValueFn = _get(binding, 'value')
+        if (_isEqual(_isNil(callValueFn), false) && _isFunction(callValueFn)) {
+          callValueFn(_get(vNode, 'componentInstance', el), _get(vNode, 'key'))
+        }
+      },
+      update (el, binding, vNode, oldVNode) {
+        console.info('update');
+        if (_isEqual(_isNil(_get(oldVNode, 'data')), false) && _isEqual(_isNil(_get(oldVNode, 'data.directives')), false)) {
+          const oldBinding = _find(_get(oldVNode, 'data.directives', []), function (directive) {
+            const name = _get(directive, 'name')
+            return _isEqual(name, directiveName)
+          })
+          const [oldValue, value] = [_get(oldBinding, 'value'), _get(binding, 'value')]
+          console.info(oldValue, value);
+          if (_isEqual(_isEmpty(oldBinding), false) && _isEqual(_isEqual(oldValue, value), false)) {
+            // 前后两个指令的 value 不一致解除值的引用
+            oldValue(null, _get(oldVNode, 'key'))
+            // 设置新的值
+            value(_get(vNode, 'componentInstance', el), _get(vNode, 'key'))
+          }
+        }
+        // 不应该有这种 update 发生
+        if (_isEqual(_isEqual(_get(vNode, 'componentInstance'), _get(oldVNode, 'componentInstance')), false) || _isEqual(_isEqual(_get(vNode, 'elm'), _get(oldVNode, 'elm')), false)) {
+          console.info('如果组件已经不一致（可能使用了动态组件）');
+          const callValueFn = _get(binding, 'value')
+          if (_isEqual(_isNil(callValueFn), false) && _isFunction(callValueFn)) {
+            console.info('如果组件已经不一致（可能使用了动态组件）');
+            callValueFn(_get(vNode, 'componentInstance'), _get(vNode, 'key'))
+          }
+        }
+      },
+      unbind (el, binding, vNode) {
+        const callValueFn = _get(binding, 'value')
+        if (_isEqual(_isNil(callValueFn), false) && _isFunction(callValueFn)) {
+          // 销毁组件解除指令的引用
+          console.info('unbind', binding.expression);
+          callValueFn(null, _get(vNode, 'key'))
+        }
+      }
+    })
+  }
+}
