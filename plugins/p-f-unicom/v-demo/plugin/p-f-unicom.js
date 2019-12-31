5db9a1f4d3ff5b9b3f29b7d5c49a193f072675d5
zhanghao <506365426@qq.com>
2019-12-31 10:07:15 +0800

    feat: unicomGroup作为 prop 属性传入时和组件内的 unicomGroup 进行合并而不是取两个数组的交集

--- plugins/p-f-unicom/v-demo/plugin/p-f-unicom.js
+++ plugins/p-f-unicom/v-demo/plugin/p-f-unicom.js
@@ -12,7 +12,7 @@ import _keys from 'lodash/keys'
 import _isFunction from 'lodash/isFunction'
 import _uniq from 'lodash/uniq'
 import _flatMap from 'lodash/flatMap'
-import _intersection from 'lodash/intersection'
+import _union from 'lodash/union'
 /**
  * @desc
  * Vue 跨组件通信插件
@@ -50,7 +50,7 @@ const emitEvent = function (method, toKey, aim, args) {
       // 分组
       let group = _get(vmMap.get(scope), 'group', [])
       let name = _get(scope, unicomGroupName, [])
-      let ns = name ? _intersection(group, _strToArray(name)) : []
+      let ns = name ? _union(group, _strToArray(name)) : []
       if ((_isEmpty(group) || _isEqual(_includes(group, toKey), false)) && _isEqual(ns.includes(toKey), false)) {
         // 目标不存在
         continue
@@ -113,7 +113,7 @@ const updateName = function (scope, nv, ov) {
   }
   // 增加新的
   if (_isEqual(_isUndefined(nv), false)) {
-    _intersection(_strToArray(nv), vmData.group).forEach(function (key) {
+    _union(_strToArray(nv), vmData.group).forEach(function (key) {
       let vms = groupForVm[key]
       if (_isUndefined(vms)) {
         vms = groupForVm[key] = []
@@ -326,7 +326,7 @@ export default {
     })
     // 自定义属性合并策略
     // 混入（mixins）时不是简单的覆盖而是追加
-    let merge = _get(Vue, 'config.optionMergeStrategies', {})
+    const merge = _get(Vue, 'config.optionMergeStrategies', {})
     merge[name] = merge[unicomGroupName] = function (parentVal, childVal) {
       let p = parentVal || []
       if (_isEqual(_isUndefined(childVal), false)) {
