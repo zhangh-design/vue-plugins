a2ea5ff86df8d9099da788646e1bb0e7c27a4467
zhangh <506365426@qq.com>
2020-04-03 14:44:13 +0800

    fix: 修改export导出形式

--- plugins/p-f-define/define/index.js
+++ plugins/p-f-define/define/index.js
@@ -1,3 +1,10 @@
-export { default as VueDefine } from './install.js'
+import VueDefine from './install.js'
+import DefineManager from './define_manager.js'
+import Define from './define.js'
+
+/* export { default as VueDefine } from './install.js'
 export { default as DefineManager } from './define_manager.js'
-export { default as Define } from './define.js'
+export { default as Define } from './define.js' */
+
+const FastVueFDefine = { VueDefine, DefineManager, Define }
+export default FastVueFDefine
